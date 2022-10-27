2daaea5c56344fa6ca09677f7912f8e290862328
Maciej Glownia <maciej.glownia@infor.com>
2022-10-27 14:03:40 +0200

    Add method in repository to get photos

--- app/src/main/java/com/glownia/maciej/photofromapi/data/PhotosRepository.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/data/PhotosRepository.kt
@@ -1,5 +1,8 @@
 package com.glownia.maciej.photofromapi.data
 
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.liveData
 import com.glownia.maciej.photofromapi.api.PhotosApi
 import javax.inject.Inject
 import javax.inject.Singleton
@@ -7,4 +10,13 @@ import javax.inject.Singleton
 @Singleton
 class PhotosRepository @Inject constructor(private val photosApi: PhotosApi) {
 
+    fun getPhotosResults() =
+        Pager(
+            config = PagingConfig(
+                pageSize = 20,
+                maxSize = 100,
+                enablePlaceholders = false
+            ),
+            pagingSourceFactory = { PhotosPagingSource(photosApi) }
+        ).liveData
 }
