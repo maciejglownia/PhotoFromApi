9eeb3c128f1d72a9efcc6e36874a44aba917365d
Maciej Glownia <maciej.glownia@infor.com>
2022-10-27 16:55:15 +0200

    Implement binding in PhotosFragment

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.glownia.maciej.photofromapi.databinding.FragmentPhotosBinding
+import com.glownia.maciej.photofromapi.ui.adapters.PhotoAdapter
 import com.glownia.maciej.photofromapi.ui.viewmodels.PhotosViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -12,11 +14,27 @@ class PhotosFragment : Fragment() {
 
     private val viewModel by viewModels<PhotosViewModel>()
 
+    private var _binding: FragmentPhotosBinding? = null
+    private val binding get() = _binding!!
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        viewModel.photos.observe(viewLifecycleOwner) {
+        _binding = FragmentPhotosBinding.bind(view)
 
+        val adapter = PhotoAdapter()
+        binding.apply {
+            recyclerView.setHasFixedSize(true)
+            recyclerView.adapter
         }
+
+        viewModel.photos.observe(viewLifecycleOwner) {
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
     }
 }
