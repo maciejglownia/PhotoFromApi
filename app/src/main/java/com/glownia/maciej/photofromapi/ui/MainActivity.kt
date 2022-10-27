f558f4e8f10d3399f9d016ca646fb759691507a7
Maciej Glownia <maciej.glownia@infor.com>
2022-10-27 17:02:16 +0200

    Refactor MainActivity

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/MainActivity.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/MainActivity.kt
@@ -1,11 +1,9 @@
 package com.glownia.maciej.photofromapi.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.glownia.maciej.photofromapi.R
-import com.glownia.maciej.photofromapi.ui.fragments.PhotosFragment
 import dagger.hilt.android.AndroidEntryPoint
-import dagger.hilt.android.HiltAndroidApp
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
@@ -13,10 +11,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, PhotosFragment.newInstance())
-                .commitNow()
-        }
     }
 }
