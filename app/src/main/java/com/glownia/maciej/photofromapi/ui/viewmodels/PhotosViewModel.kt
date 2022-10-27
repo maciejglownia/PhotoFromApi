a59468139a868609e2d2d9a0e5071db5c66bf034
Maciej Glownia <maciej.glownia@infor.com>
2022-10-27 14:31:09 +0200

    Add function in view model class to get photo results

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/viewmodels/PhotosViewModel.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/viewmodels/PhotosViewModel.kt
@@ -1,6 +1,8 @@
 package com.glownia.maciej.photofromapi.ui.viewmodels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.cachedIn
 import com.glownia.maciej.photofromapi.data.PhotosRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
@@ -10,4 +12,9 @@ class PhotosViewModel @Inject constructor(
     private val repository: PhotosRepository
 ) : ViewModel() {
 
+    val photos = repository.getPhotosResults()
+
+    fun getPhotos() {
+        repository.getPhotosResults().cachedIn(viewModelScope)
+    }
 }
