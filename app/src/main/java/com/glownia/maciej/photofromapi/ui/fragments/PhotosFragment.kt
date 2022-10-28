4d4c9a94962721c39b1f2bbfe33e6f8330c3e306
Maciej Glownia <maciej.glownia@infor.com>
2022-10-28 12:37:52 +0200

    Add search item in menu for future searching

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
@@ -1,14 +1,21 @@
 package com.glownia.maciej.photofromapi.ui.fragments
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.view.View
+import androidx.core.view.MenuHost
+import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
 import com.glownia.maciej.photofromapi.R
 import com.glownia.maciej.photofromapi.databinding.FragmentPhotosBinding
 import com.glownia.maciej.photofromapi.ui.adapters.PhotoAdapter
 import com.glownia.maciej.photofromapi.ui.adapters.PhotoLoadStateAdapter
 import com.glownia.maciej.photofromapi.ui.viewmodels.PhotosViewModel
+import com.google.android.material.snackbar.Snackbar
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,6 +27,25 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
     private val binding get() = _binding!!
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        val menuHost: MenuHost = requireActivity()
+
+        menuHost.addMenuProvider(object : MenuProvider {
+            override fun onCreateMenu(menu: Menu, menuInflater: MenuInflater) {
+                menuInflater.inflate(R.menu.menu_photos, menu)
+            }
+
+            override fun onMenuItemSelected(menuItem: MenuItem): Boolean {
+                return when (menuItem.itemId) {
+                    R.id.search_icon -> {
+                        Snackbar.make(view, "Search icon works!", Snackbar.LENGTH_LONG)
+                            .setAction("Action", null).show()
+                        true
+                    }
+                    else -> false
+                }
+            }
+        }, viewLifecycleOwner, Lifecycle.State.RESUMED)
+
         super.onViewCreated(view, savedInstanceState)
 
         _binding = FragmentPhotosBinding.bind(view)
@@ -30,7 +56,7 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
             // To simulate bad internet connection in emulator go to 3 dots -> Cellular -> Set up Network type to GPRS
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = PhotoLoadStateAdapter { adapter.retry() },
-                footer = PhotoLoadStateAdapter {adapter.retry() },
+                footer = PhotoLoadStateAdapter { adapter.retry() },
             )
         }
 
