740a6f5ffb191ff90b39ebf523f2b1967856df72
Maciej Glownia <maciej.glownia@infor.com>
2022-10-30 08:50:40 +0100

    Add load state listener to display retry button to user when there is the internet connection problem

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/fragments/PhotosFragment.kt
@@ -9,9 +9,11 @@ import androidx.appcompat.widget.SearchView
 import androidx.appcompat.widget.SearchView.OnQueryTextListener
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
+import androidx.paging.LoadState
 import com.glownia.maciej.photofromapi.R
 import com.glownia.maciej.photofromapi.databinding.FragmentPhotosBinding
 import com.glownia.maciej.photofromapi.ui.adapters.PhotoAdapter
@@ -60,15 +62,37 @@ class PhotosFragment : Fragment(R.layout.fragment_photos) {
         val adapter = PhotoAdapter()
         binding.apply {
             recyclerView.setHasFixedSize(true)
+            recyclerView.itemAnimator = null
             // To simulate bad internet connection in emulator go to 3 dots -> Cellular -> Set up Network type to GPRS
             recyclerView.adapter = adapter.withLoadStateHeaderAndFooter(
                 header = PhotoLoadStateAdapter { adapter.retry() },
                 footer = PhotoLoadStateAdapter { adapter.retry() },
             )
+            buttonRetry.setOnClickListener {
+                adapter.retry()
+            }
         }
 
         viewModel.photos.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
+        }
+
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                recyclerView.isVisible = loadState.source.refresh is LoadState.NotLoading
+                buttonRetry.isVisible = loadState.source.refresh is LoadState.Error
+                textViewError.isVisible = loadState.source.refresh is LoadState.Error
+
+            if (loadState.source.refresh is LoadState.NotLoading &&
+                    loadState.append.endOfPaginationReached &&
+                    adapter.itemCount < 1) {
+                recyclerView.isVisible = false
+                textViewEmpty.isVisible = true
+            } else {
+                textViewEmpty.isVisible = false
+            }
+            }
         }
     }
 
