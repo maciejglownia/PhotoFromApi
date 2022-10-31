99592a8f2c37f074bfbd3e2a546cadb977af355a
Maciej Glownia <maciej.glownia@infor.com>
2022-10-31 22:58:09 +0100

    Handle nav controller - back functionality on the toolbar

--- app/src/main/java/com/glownia/maciej/photofromapi/ui/MainActivity.kt
+++ app/src/main/java/com/glownia/maciej/photofromapi/ui/MainActivity.kt
@@ -2,14 +2,32 @@ package com.glownia.maciej.photofromapi.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.glownia.maciej.photofromapi.R
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment_main) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        // Connect app bar to nav graph
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        setupActionBarWithNavController(navController, appBarConfiguration)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
